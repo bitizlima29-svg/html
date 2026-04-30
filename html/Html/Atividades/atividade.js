e509a39f0735e98383c1579923bcaf861ee0e4f5
bitizlima29-svg <bitizlima.29@gmail.com>
2026-04-30 11:00:04 -0300

    atividade14

--- html/Html/Atividades/atividade.js
+++ html/Html/Atividades/atividade.js
@@ -2,3 +2,6 @@ function excluir(){
     return window.confirm('Você tem certeza que deseja excluir este item?')
 
 }
+function pagnova(){
+    return window.confirm('Deseja abrir uma nova página?')
+}
